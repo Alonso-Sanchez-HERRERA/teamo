e47ac8cf45398809c41706c1b8495eec2e0c44e5
Alonso-Sanchez-HERRERA <julioalonshjr@gmail.com>
2025-07-10 03:00:06 -0600

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,8 @@
+const textoFondo = document.getElementById("textoFondo");
+let texto = "";
+
+for (let i = 1; i < 1000; i++) {
+  texto += "TE AMO MUCHO     TE AMO ALEJANDRITA     TE AMO MUCGROSA     TE ASMOMUSHO     TE AMO MUCHO     TE AMO ALEJANDRITA     TE AMO MUGROSA     TE ASMOMUSHO     TE AMO MUCHO     TE AMO ALEJANDRITA     TE AMO MUGROSA     TE ASMOMUSHO\n";
+}
+
+textoFondo.textContent = texto;
